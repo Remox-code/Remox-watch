ecfff2010820426dab9a8a3500e494fa349d6376
Remox-code <remoxafficial.4@gmail.com>
2026-09-10 11:47:44 +0330

    Update website

--- config.js
+++ config.js
@@ -1,5 +1,3 @@
 window.WATCHFLOW_CONFIG = {
   TMDB_API_KEY: "e83a4d78ae885ef9c2d5b1ff67554ec8",
-  SUPABASE_URL: "https://tescrgvihahmfbiczlpu.supabase.co",
-  SUPABASE_ANON_KEY: "sb_publishable_y2XBdwYqz7f12DnvA-ciIQ_UEXpWfeb",
 };
